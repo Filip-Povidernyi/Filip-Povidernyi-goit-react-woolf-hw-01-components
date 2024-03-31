64892eaef683166556e54942ded7aac78cc2a724
Filip-Povidernyi <p.poviderniy@gmail.com>
2024-03-31 15:26:47 +0300

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import dataProfile from '../data/user.json'
+
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
     >
-      React homework template
+      <Profile data={dataProfile}/>
     </div>
   );
 };
+export default App
